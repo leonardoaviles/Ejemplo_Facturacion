8dc957f45dec8d2a1306d52b8ef76183b050c9ed
Leonardo AvilesMeneses <leonardo.aviles.meneses@gmail.com>
2023-03-08 17:22:13 -0600

    se suben cambios

--- Ejercicio2/ejercicio2.java
+++ Ejercicio2/ejercicio2.java
@@ -8,12 +8,21 @@ public class ejercicio2 {
     
     public static void main(String[] args) {
 
+        Persona persona1 = new Persona("Carlos","Sanchez");
+        Persona persona2 = new Persona("Juan","Mendez");
+        Persona persona3 = new Persona("Pedro","Linares");
+        Persona persona4 = new Persona("Luis","Marquez");
+        Persona persona5 = new Persona("Alejandro","Parra");
+
+
         //Se crea la lista de personas
-        List<Persona> personas = Arrays.asList(new Persona("Carlos","Sanchez"),
-                                               new Persona("Juan","Mendez"),
-                                               new Persona("Pedro","Linares"),
-                                               new Persona("Luis","Marquez"),
-                                               new Persona("Alejandro","Parra"));
+        List<Persona> personas = new ArrayList<Persona>();
+
+        personas.add(persona1);
+        personas.add(persona2);
+        personas.add(persona3);
+        personas.add(persona4);
+        personas.add(persona5);
 
         //Se utiliza la propiedad sort de la clase lista para poder iterar las personas y se utiliza la clase comparator para
         //poder comparar los valores
