8a0ac42f39cc6177c7219141a339c9d0ecbe8c40
Leonardo AvilesMeneses <leonardo.aviles.meneses@gmail.com>
2023-03-08 17:18:49 -0600

    se suben cambios

--- Ejercicio2/ejercicio2.java
+++ Ejercicio2/ejercicio2.java
@@ -17,7 +17,7 @@ public class ejercicio2 {
 
         //Se utiliza la propiedad sort de la clase lista para poder iterar las personas y se utiliza la clase comparator para
         //poder comparar los valores
-        personas.sort(Comparator.comparing(Persona::getNombre).thenComparing(Persona::getApellido));
+        personas.sort(Comparator.comparing(Persona::getNombre));
 
         System.out.println("Comparando por nombre");
 
@@ -28,7 +28,7 @@ public class ejercicio2 {
 
         System.out.println("\nComparando por apellido");
 
-        personas.sort(Comparator.comparing(Persona::getApellido).thenComparing(Persona::getNombre));
+        personas.sort(Comparator.comparing(Persona::getApellido));
 
         for(Persona personaFinal : personas){
             System.out.println("Nombre: "+ personaFinal.getNombre() + " Apellido: "+ personaFinal.getApellido());
@@ -36,7 +36,7 @@ public class ejercicio2 {
 
         System.out.println("\nComparando por apellido a la inversa");
 
-        personas.sort(Comparator.comparing(Persona::getApellido).reversed().thenComparing(Persona::getNombre));
+        personas.sort(Comparator.comparing(Persona::getApellido).reversed());
 
         for(Persona personaFinal : personas){
             System.out.println("Nombre: "+ personaFinal.getNombre() + " Apellido: "+ personaFinal.getApellido());
