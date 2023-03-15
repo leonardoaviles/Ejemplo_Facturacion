2ba7e87511169353c1ea8e97781b1f64518951c2
Leonardo AvilesMeneses <leonardo.aviles.meneses@gmail.com>
2023-03-14 21:21:28 -0600

    Se suben cambios para la creacion de la base de datos

---
+++ facturacion/src/main/java/com/facturacion/facturacion/Model/ClientsModel.java
@@ -0,0 +1,103 @@
+package com.facturacion.facturacion.Model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "clients")
+public class ClientsModel implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pkClient;
+    @NonNull
+    @Column(name = "nombre")
+    private String name;
+    @NonNull
+    @Column(name = "lastName")
+    private String lastName;
+    @NonNull
+    @Column(name = "adress")
+    private String adress;
+    @NonNull
+    @Column(name = "phone")
+    private String phone;
+    @NonNull
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "clientsModel" , cascade = CascadeType.ALL)
+    private List <FacturaModel> facturas;
+
+    public ClientsModel(int pkclient, String name, String lastName, String adress, String phone, String email) {
+        this.pkClient = pkclient;
+        this.name = name;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.phone = phone;
+        this.email = email;
+        this.facturas = new ArrayList<FacturaModel>();
+    }
+
+    public int getPkClient() {
+        return pkClient;
+    }
+    public void setPkClient(int pkclient) {
+        this.pkClient = pkclient;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAdress() {
+        return adress;
+    }
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public List<FacturaModel> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<FacturaModel> facturas) {
+        this.facturas = facturas;
+    }
+}
+
+
---
+++ facturacion/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into clients (nombre,lastName,adress,phone,email) values ('Leonardo','Aviles','patito','5545678912','leo@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('Carlos','Alvarez','patito','7889451623','carlos@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('Juan','Arreola','patito','7891255456','juan@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('Martin','Velazquez','patito','6789125545','martin@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('Miguel','Mendez','patito','5545678912','miguel@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('David','Gonzalez','patito','5545627891','david@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('Raul','Arreola','patito','8912554567','raul@gmail.com')
+insert into clients (nombre,lastName,adress,phone,email) values ('Oscar','Marquez','patito','5589124567','oscar@gmail.com')
